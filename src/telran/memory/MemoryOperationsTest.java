6f420c8307bfc2e5bd3887326fb1512ab562fd25
Дмитрий <rubanovdp@gmail.com>
2022-12-17 13:29:33 +0200

    +add memoryOperation class and test

---
+++ src/telran/memory/MemoryOperationsTest.java
@@ -0,0 +1,23 @@
+package telran.memory;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MemoryOperationsTest {
+	byte ar[];
+
+	@Test
+	void maxMemoryTesttest() {
+		int maxMemory = MemoryOperations.getMaxAvailableMemory();
+		ar = new byte[maxMemory];
+		ar = null;
+		boolean filException = false;
+		try {
+			ar = new byte[maxMemory + 1];
+		} catch(Throwable e) {
+			filException = true;
+		}
+assertTrue(filException);
+	}
+}
--- src/telran/shapes/SquareLeftTriangle.java
+++
@@ -1,7 +0,0 @@
-package telran.shapes;
-
-public class SquareLeftTriangle extends SquareTriangle {
-	public SquareLeftTriangle(int width, boolean isLeftDiagonal) {
-		super(width, true);
-	}
-}
